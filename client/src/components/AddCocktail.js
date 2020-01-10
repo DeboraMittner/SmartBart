0711bd794f13e2a139d7a37b6477ef9856649b1c
DeboraMittner <debora.mittner@gmail.com>
2020-01-10 05:28:21 +0100

    change pump design

--- client/src/components/AddCocktail.js
+++ client/src/components/AddCocktail.js
@@ -48,7 +48,7 @@ handleBlur = () => {
 
   componentDidMount(){
     axios
-    .get("http://localhost:4000/pumps")
+    .get("http://localhost:3000/pumps")
     .then(response => {
       this.setState({pumps: response.data});
       console.log(response);
@@ -98,7 +98,6 @@ handleBlur = () => {
                   <Input
                     width="42"
                     value={this.value}
-                    marginTop="50px"
                     onChange={this.handleInputChange}
                     onBlur={this.handleBlur}
                     inputProps={{
--- client/src/components/ChangePumpDialog.js
+++ client/src/components/ChangePumpDialog.js
@@ -1,4 +1,4 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,13 +7,14 @@ import SaveIcon from '@material-ui/icons/Save';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import axios from "axios";
+import '../App.css';
 
 
 
 class ChangePumpDialog extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    
+
     this.state = {
       pumps: [],
       visible: false,
@@ -21,31 +22,31 @@ class ChangePumpDialog extends Component {
       drinkTypes: 'alcohol'
     }
   }
-  
+
 
   handleClickOpen = () => {
-    this.setState({open: true});
+    this.setState({ open: true });
   };
 
   save = () => {
     axios.put('http://localhost:4000/pumps', {
       pumps: this.state.pumps
     })
-    .then(response => {
-      console.log(response);
-    });
-    this.setState({open: false});
+      .then(response => {
+        console.log(response);
+      });
+    this.setState({ open: false });
   };
 
   handlePumpNameChange = (pumpId, event) => {
     var tempPumps = this.state.pumps;
 
     tempPumps.forEach((pump) => {
-      if(pump.pump === pumpId) {
+      if (pump.pump === pumpId) {
         pump.name = event.target.value.toLowerCase();
       }
     });
-    this.setState({pump: tempPumps});
+    this.setState({ pump: tempPumps });
 
   }
 
@@ -53,84 +54,89 @@ class ChangePumpDialog extends Component {
     var tempPumps = this.state.pumps;
 
     tempPumps.forEach((pump) => {
-      if(pump.pump === pumpId) {
+      if (pump.pump === pumpId) {
         pump.alcohol = !pump.alcohol
       }
     });
-    this.setState({pump: tempPumps});
+    this.setState({ pump: tempPumps });
   };
 
-componentDidMount(){
+  componentDidMount() {
     axios
-    .get("http://localhost:4000/pumps")
-    .then(response => {
-      this.setState({pumps: response.data});
-      console.log('WooooWi');
-      console.log(response);
-    });
+      .get("http://localhost:4000/pumps")
+      .then(response => {
+        this.setState({ pumps: response.data });
+        console.log(response);
+      });
   }
 
-render(){
-  return (
-    <div>
-      <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
-        Change Pump Settings
+  render() {
+    return (
+      <div className='AddButton'>
+        <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+          Change Pump Settings
       </Button>
-      <Dialog open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Change Pumps</DialogTitle>
-        
-        <DialogActions>
- 
-      {this.state.pumps.map((pump, index) => {
-        return (
-          <form 
-          width= '250'
-          noValidate 
-          autoComplete="off"
-          key={pump.pump}>
-           <p>{pump.pump + 1}</p>
-           <TextField 
-            id="standard-basic" 
-            label="Drink" 
-            defaultValue={pump.name}
-            onChange={(event) => this.handlePumpNameChange(pump.pump, event)}
-            />
-           <TextField
-            select
-            value={pump.alcohol ? "Alcohol" : "No-Alcohol"}
-            onChange={(event) => this.handleAlcoholChange(pump.pump, event)}
+        <Dialog 
+        maxWidth='xl' 
+        open={this.state.open} 
+        onClose={this.handleClose} 
+        aria-labelledby="form-dialog-title"
+        padding='200px'
+        >
+          <DialogTitle>Change Pumps</DialogTitle>
+
+          <DialogActions className='ChangePump'>
+
+            {this.state.pumps.map((pump, index) => {
+              return (
+                <form
+                  width='250'
+                  noValidate
+                  autoComplete="off"
+                  key={pump.pump}>
+                  <p>{pump.pump + 1}</p>
+                  <TextField
+                    id="standard-basic"
+                    label="Ingredient"
+                    defaultValue={pump.name}
+                    onChange={(event) => this.handlePumpNameChange(pump.pump, event)}
+                  />
+                  <TextField
+                    select
+                    value={pump.alcohol ? "Alcohol" : "No-Alcohol"}
+                    onChange={(event) => this.handleAlcoholChange(pump.pump, event)}
+                  >
+
+                    <MenuItem key={'alcohol'} value={'Alcohol'}>
+                      Alcohol
+              </MenuItem>
+                    <MenuItem key={'noalcohol'} value={'No-Alcohol'}>
+                      No-Alcohol
+              </MenuItem>
+
+                  </TextField>
+
+                </form>
+
+              );
+            }
+
+            )}
+
+
+            <Button
+              onClick={this.save}
+              variant="contained"
+              color="primary"
+              size="large"
+              startIcon={<SaveIcon />}
             >
-        
-              <MenuItem key={'alcohol'} value={'Alcohol'}>
-                Alcohol
-              </MenuItem>
-              <MenuItem key={'noalcohol'} value={'No-Alcohol'}>
-                No-Alcohol
-              </MenuItem>
-           
-          </TextField>
-  
-          </form>
-
-        );
-      }
-      
-      )}
-       
-
-          <Button 
-          onClick={this.save} 
-          variant="contained"
-          color="primary"
-          size="large"
-          startIcon={<SaveIcon />}
-          >
-            Save
+              
           </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
-}
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
 }
 export default ChangePumpDialog;
